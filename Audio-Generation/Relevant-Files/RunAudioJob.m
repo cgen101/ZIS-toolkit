ccd19913dbbaad3edd5887d87bc86c282dc76876
cgen101 <c_g638@txstate.edu>
2024-04-14 13:57:51 -0500

    Update RunAudioJob

--- Audio-Generation/Relevant-Files/RunAudioJob.m
+++ Audio-Generation/Relevant-Files/RunAudioJob.m
@@ -1,11 +1,11 @@
 % RunAudioJob.m
 filePath1 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\non_colocated_highway.flac";
 filePath2 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\non_colocated_idle_2.flac";
-resultPath = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\Relevant-Files";
+ioPath = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\Relevant-Files";
 
 filePath3 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\colocated_city.flac";
-filePath4 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\colocated_city_2.flac"
+filePath4 = "C:\Users\chlo\Documents\Spring 24\Security (CS4371)\Project\ZIS-toolkit\Audio-Generation\colocated_city_2.flac";
 
-newAudioJob(filePath1, filePath2, resultPath);
+newAudioJob(filePath1, filePath2, ioPath);
 
-newAudioJob(filePath3, filePath4, resultPath);
+newAudioJob(filePath3, filePath4, ioPath);
